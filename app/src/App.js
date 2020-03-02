2b1982ea25c0616273b6a5937062a26d90ba9481
Ruben Sitbon <ruben.sitbon@outlook.com>
2020-03-02 20:29:11 +0100

    Design first page

--- app/src/App.js
+++ app/src/App.js
@@ -1,19 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <h1>JoyPi IHM - Centrale Lille</h1>
+      </header>
+      <div className="App-body">
         <a
           className="App-link"
           href="/html/test.html"
           rel="noopener noreferrer"
         >
-          Go to the Text File
+          Go to the Test File
         </a>
-      </header>
+      </div>
     </div>
   );
 }
