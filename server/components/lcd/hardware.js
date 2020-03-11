6d9d155e9f0c45e2398b44164a29963137e9598d
Bro-nes <marquisvalentin@hotmail.fr>
2020-03-11 18:13:11 +0100

    add lcd compenent

---
+++ server/components/lcd/hardware.js
@@ -0,0 +1,29 @@
+
+// Function to get values from the board (get the output)
+function get() {
+  return 1;
+}
+
+// Function to interact with Input values of the board
+function post(ligne1, ligne2) {
+	var {PythonShell} = require('python-shell');
+
+	var command = 'open1';
+	var comport = 6;
+
+	let options = {
+	    args: [ligne1,ligne2]
+	};
+
+	PythonShell.run('hardware.py', options, function (err) {
+	  if (err) throw err;
+	  console.log('finished');
+	});
+  return 1;
+}
+
+// Syntax to export functions
+module.exports = {
+  get,
+  post
+};
---
+++ server/components/lcd/hardware.py
@@ -0,0 +1,38 @@
+import time
+import board
+import busio
+import adafruit_character_lcd.character_lcd_i2c as character_lcd
+import sys
+
+lcd_columns = 16
+lcd_rows    = 2
+
+i2c = busio.I2C(board.SCL, board.SDA)
+
+lcd = character_lcd.Character_LCD_I2C(i2c, lcd_columns, lcd_rows)
+
+
+try:
+
+	if(len(sys.argv)>2):
+
+		lcd.backlight = True
+
+		lcd.clear()
+
+		ligne1 = sys.argv[1][0:16]
+		ligne2 = sys.argv[2][0:16]
+
+		lcd.message = ligne1+"\n"+ligne2
+
+		time.sleep(5)
+
+		lcd.clear()
+		lcd.backlight = False
+
+	else:
+		print ('Pas assez d arguments')
+
+except KeyboardInterrupt:
+    lcd.clear()
+    lcd.backlight = False
